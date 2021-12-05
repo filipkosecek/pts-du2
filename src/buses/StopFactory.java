c0261bbe26d7f564fa19a4b6396bdcf2df77c637
tanahusarova <83506375+tanahusarova@users.noreply.github.com>
2021-12-05 11:02:34 +0100

    Update StopFactory.java

--- src/buses/StopFactory.java
+++ src/buses/StopFactory.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+// preco si si nedal v tych factories createStop a createLine dokopy? co ked ti niekto zavola stopku z pamate a line z databazy? 
+// Ako asi by to robilo potom na urovni logiky co ma, ale nemalo by byt zaistene aby to bolo jednotne? lebo ja som to tak vnimala :D
+
 public class StopFactory implements AbstractStopFactory{
     private final HashMap<StopName, ArrayList<LineName>> stops;
 
